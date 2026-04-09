bcbb49d4ffdebbb32ba62930fe9e3c32996c0d24
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-09 19:12:33 +0200

    Creato un ciclo for per assegnare alle proprietà puntiSubiti e falliSubiti valori random fra 1 e 100.

--- Squadre_di_calcio/script.js
+++ Squadre_di_calcio/script.js
@@ -2,32 +2,41 @@
 const squadreDiClacio = [
     {
         "nome": "squadraDiTopolino",
-        "puntiFatti": 0
+        "puntiFatti": 0,
         "falliSubiti": 0
-    }
+    },
     {
         "nome": "squadraDiPippo",
-        "puntiFatti": 0
+        "puntiFatti": 0,
         "falliSubiti": 0
-    }
+    },
     {
         "nome": "squadraDiPluto",
-        "puntiFatti": 0
+        "puntiFatti": 0,
         "falliSubiti": 0
-    }
+    },
     {
         "nome": "squadraDiMinnie",
-        "puntiFatti": 0
+        "puntiFatti": 0,
         "falliSubiti": 0
-    }
+    },
     {
-        "nome": "squadraDiPietro",
-        "puntiFatti": 0
+    "nome": "squadraDiPietro",
+        "puntiFatti": 0,
         "falliSubiti": 0
-    }
+    },
     {
-        "nome": "squadraDiPaperino",
-        "puntiFatti": 0
+    "nome": "squadraDiPaperino",
+        "puntiFatti": 0,
         "falliSubiti": 0
     }
 ];
+
+// Ciclo dentro ad un for le squadre di calcio per assegnare valori random a punti fatti
+// e falli subiti
+
+for ( let i = 0; i < squadreDiClacio.length; i++) {
+        squadreDiClacio[i].puntiFatti = (Math.floor (Math.random() * 100 ) +1)
+        squadreDiClacio[i].falliSubiti = (Math.floor (Math.random() * 100 ) +1)
+    
+};
