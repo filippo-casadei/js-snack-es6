32489b80911433406844cc755b3415e54e12d33f
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-09 18:54:46 +0200

    Modificato il file script.js: - creato array di oggetti squadre di calcio con relative proprietà.

---
+++ Squadre_di_calcio/script.js
@@ -0,0 +1,33 @@
+// Creo array per le squadre di calcio con le relative proprietà
+const squadreDiClacio = [
+    {
+        "nome": "squadraDiTopolino",
+        "puntiFatti": 0
+        "falliSubiti": 0
+    }
+    {
+        "nome": "squadraDiPippo",
+        "puntiFatti": 0
+        "falliSubiti": 0
+    }
+    {
+        "nome": "squadraDiPluto",
+        "puntiFatti": 0
+        "falliSubiti": 0
+    }
+    {
+        "nome": "squadraDiMinnie",
+        "puntiFatti": 0
+        "falliSubiti": 0
+    }
+    {
+        "nome": "squadraDiPietro",
+        "puntiFatti": 0
+        "falliSubiti": 0
+    }
+    {
+        "nome": "squadraDiPaperino",
+        "puntiFatti": 0
+        "falliSubiti": 0
+    }
+];
