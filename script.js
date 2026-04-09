62ff39723caec9add799f3487600cb1ee3aa2b2c
Filippo Casadei <casadeifilippo91@gmail.com>
2026-04-09 16:45:50 +0200

    Modificato file script.js: - creato array di oggetti.

---
+++ script.js
@@ -0,0 +1,37 @@
+// Creo un array di oggetti ogni oggetti (di bici da corsa) ogni oggetto avrà 2 proprietà:
+// nome e peso. Ho scelto di commentare l'unità di misura (kg) perchè mi sembrava giusto
+// specificarla, ma mi è inutile scriverla non commentata, interferirebbe solo col calcolo
+// in quanto non avrei un nu numero ma una stringa.
+
+const tutteLeBici = [
+{  
+    "id": "Bici Rossa",
+    "nome": "Rossa",
+    "peso": 1 // kg
+
+},
+{  
+    "id": "Bici Rosa",
+    "nome": "Rosa",
+    "peso": 2 // kg
+
+},
+{  
+    "id": "Bici Verde",
+    "nome": "Verde",
+    "peso": 9 // kg
+
+},
+{  
+    "id": "Bici Gialla",
+    "nome": "Gialla",
+    "peso": 19 // kg
+
+},
+{  
+    "id": "Bici Blu",
+    "nome": "Blu",
+    "peso": 7 // kg
+
+},
+];
